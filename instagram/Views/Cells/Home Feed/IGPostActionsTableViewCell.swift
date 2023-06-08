7bc78f778c382a1820f88bd020985b2cc37fc592
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-06-08 13:58:49 +0300

    Making the home feed

--- instagram/Views/Cells/Home Feed/IGPostActionsTableViewCell.swift
+++ instagram/Views/Cells/Home Feed/IGPostActionsTableViewCell.swift
@@ -14,7 +14,7 @@ class IGPostActionsTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+        contentView.backgroundColor = .systemGreen
     }
     
     required init?(coder: NSCoder) {
